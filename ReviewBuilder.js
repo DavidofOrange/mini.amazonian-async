87b7ce21ea01404415bf6375c17a7e4eed1a4c71
Jay <jaysutham@gmail.com>
2021-04-08 16:59:11 +0900

    fixed mia2

--- ReviewBuilder.js
+++ ReviewBuilder.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const { readFile, produceResult } = require("./helpers");
+const { produceResult } = require("./helpers");
 
 class ReviewBuilder {
   buildReviewsSync() {
